02266a68d98ac6229366ac51b5fe6409261da846
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-12-09 19:50:17 -0800

    added rockets

--- schema.js
+++ schema.js
@@ -45,12 +45,29 @@ const RootQuery = new GraphQLObjectType({
             }
         },
         launch: {
-            type: LaunchType,
+            type: RocketType,
             args: {
                 flight_number: {type: GraphQLInt }
             },
             resolve(parent, args) {
                 return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}/`)
+                .then(res => res.data )
+            }
+        }, 
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve( parent , args) { 
+                return axios.get('https://api.spacexdata.com/v3/rockets/')
+                .then( res => res.data)
+            }
+        },
+        rocket: {
+            type: RocketType,
+            args: {
+                id: {type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}/`)
                 .then(res => res.data )
             }
         }
