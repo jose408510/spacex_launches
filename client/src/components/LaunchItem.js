3168d276e9e1e358e015ea0e7bb2ef8bf992ac07
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-12-11 15:23:28 -0800

    fix all bug and errors in code

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import className from 'classnames'
+import classNames from 'classnames'
 
 function LaunchItem({ launch: {
-    flight_number , mission_name , launch_date_local, launch_sucess }}) 
+    flight_number , mission_name , launch_date_local, launch_success }}) 
     {
   return (
     <div className="card card-body mb-3">
       <div className="row"> 
         <div className="col-md-9">
             <h4> Mission: <span className={classNames({
-                'text-sucess' : launch_sucess,
-                'text-danger': !launch_sucess
+                'text-success': launch_success,
+                'text-danger': !launch_success
             })}
             >{ mission_name } </span> </h4>
             <p>Date: { launch_date_local }</p>
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -21,18 +21,16 @@ export class Launches extends Component {
             <Fragment>
                 <h1 className="display-4 my-3"> Lanches</h1>
                 <Query query={LAUNCHES_QUERY}>
-                    {
-                       ({ loading , error , data }) => {
-                            if(loading) return <h4>Loading...</h4>
-                            if(error) console.log(error)
-                            console.log(data)
-                            return <Fragment>
-                                {
-                                    data.launches.map(launch => {
-                                        // <LaunchItem key={launch.flight_number} launch={launch} />
-                                    })
-                                }
-                            </Fragment>
+                {({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
+            return (
+                <Fragment>
+                  {data.launches.map(launch => (
+                    <LaunchItem key={launch.flight_number} launch={launch} />
+                  ))}
+                </Fragment>
+            )
                        }}
                 </Query>
             </Fragment>
