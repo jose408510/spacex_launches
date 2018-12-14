897d500db43acb8fa5107fd584eae18b7aeee629
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-12-13 18:26:18 -0800

    added changes

--- schema.js
+++ schema.js
@@ -20,7 +20,6 @@ const LaunchType = new GraphQLObjectType({
       rocket: { type: RocketType }
     })
   });
-  
   // Rocket Type
   const RocketType = new GraphQLObjectType({
     name: 'Rocket',
